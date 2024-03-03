33c333b31ba22fb327b135ed052a5762acf03daf
Semyon <semyonkarabanov@gmail.com>
2024-03-03 16:17:55 +0300

    init

--- app/src/main/java/com/example/mirea_prac_1/MainActivity.java
+++ app/src/main/java/com/example/mirea_prac_1/MainActivity.java
@@ -2,12 +2,16 @@ package com.example.mirea_prac_1;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.ActionBar;
 import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 
 public class MainActivity extends AppCompatActivity {
@@ -25,8 +29,52 @@ public class MainActivity extends AppCompatActivity {
         startActivity(intent);
 
     }
+    public void setUpActionBar() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.HONEYCOMB) {
+            ActionBar actionBar = getActionBar();
+            actionBar.setDisplayHomeAsUpEnabled(true);
+        }
 
+    }
 
+    private void show_Toast(String message) {
+        Toast.makeText(this, message, Toast.LENGTH_SHORT).show();
+    }
+    protected void onStart() {
+        super.onStart();
+        show_Toast("Start now");
+        Log.d("test1","Start now");
+    }
+    protected void onResume() {
+        super.onResume();
+        show_Toast("Resume now");
+        Log.d("test1","Resume now");
+    }
+    protected void onPause() {
+        super.onPause();
+        show_Toast("Resume now");
+        Log.d("test1","Pause now");
+    }
+    protected void onStop() {
+        super.onStop();
+        show_Toast("Stop now");
+        Log.d("test1","Stop now");
+    }
+    protected void onRestart() {
+        super.onRestart();
+        show_Toast("Restart now");
+        Log.d("test1","Restart now");
+    }
+    protected void onDestroy() {
+        super.onDestroy();
+        show_Toast("Destroy now");
+        Log.d("test1","Destroy now");
+    }
+    public void onSaveInstanceState(Bundle savedInstanceState) {
+        super.onSaveInstanceState(savedInstanceState);
+        show_Toast("onSaveInstance");
+        Log.d("test1","OnSaveInstance");
+    }
 
 
     }
