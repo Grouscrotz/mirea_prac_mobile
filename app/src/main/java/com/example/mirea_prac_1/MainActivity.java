dc164c38b5381f4b2478e444690a98ea44606813
Semyon <semyonkarabanov@gmail.com>
2024-03-10 19:03:55 +0300

    init

--- app/src/main/java/com/example/mirea_prac_1/MainActivity.java
+++ app/src/main/java/com/example/mirea_prac_1/MainActivity.java
@@ -1,13 +1,17 @@
 package com.example.mirea_prac_1;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
+import androidx.constraintlayout.widget.ConstraintSet;
 
 import android.app.ActionBar;
 import android.content.Intent;
 import android.os.Build;
 import android.os.Bundle;
 import android.util.Log;
+import android.util.TypedValue;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
@@ -19,63 +23,35 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-    }
+        setContentView(R.layout.second_layout);
 
-    public void sendMessage(View v) {
-        Intent intent = new Intent(this, SecondActivity.class);
-        String message = ((EditText)findViewById(R.id.edittext)).getText().toString();
-        intent.putExtra("extra_message",message);
-        startActivity(intent);
+
+        ConstraintLayout constraintLayout = new ConstraintLayout(this);
+        EditText editText = new EditText(this);
+        editText.setHint("Введите Email");
+        editText.setId(View.generateViewId());
+
+        Button button = new Button(this);
+        button.setText("Отправить");
+        button.setId(View.generateViewId());
+
+        ConstraintLayout.LayoutParams editTextLayout = new ConstraintLayout.LayoutParams
+                (ConstraintLayout.LayoutParams.WRAP_CONTENT , ConstraintLayout.LayoutParams.WRAP_CONTENT);
+        editTextLayout.leftToLeft = ConstraintLayout.LayoutParams.PARENT_ID;
+        editTextLayout.topToTop = ConstraintLayout.LayoutParams.PARENT_ID;
+        editTextLayout.rightToLeft = button.getId();
+        editText.setLayoutParams(editTextLayout);
+        constraintLayout.addView(editText);
+
+        ConstraintLayout.LayoutParams buttonLayout = new ConstraintLayout.LayoutParams
+                (ConstraintLayout.LayoutParams.WRAP_CONTENT , ConstraintLayout.LayoutParams.WRAP_CONTENT);
+        buttonLayout.leftToRight = editText.getId();
+        buttonLayout.topToTop = ConstraintLayout.LayoutParams.PARENT_ID;
+        button.setLayoutParams(buttonLayout);
+        constraintLayout.addView(button);
+        setContentView(constraintLayout);
 
     }
-    public void setUpActionBar() {
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.HONEYCOMB) {
-            ActionBar actionBar = getActionBar();
-            actionBar.setDisplayHomeAsUpEnabled(true);
-        }
-
-    }
-
-    private void show_Toast(String message) {
-        Toast.makeText(this, message, Toast.LENGTH_SHORT).show();
-    }
-    protected void onStart() {
-        super.onStart();
-        show_Toast("Start now");
-        Log.d("test1","Start now");
-    }
-    protected void onResume() {
-        super.onResume();
-        show_Toast("Resume now");
-        Log.d("test1","Resume now");
-    }
-    protected void onPause() {
-        super.onPause();
-        show_Toast("Resume now");
-        Log.d("test1","Pause now");
-    }
-    protected void onStop() {
-        super.onStop();
-        show_Toast("Stop now");
-        Log.d("test1","Stop now");
-    }
-    protected void onRestart() {
-        super.onRestart();
-        show_Toast("Restart now");
-        Log.d("test1","Restart now");
-    }
-    protected void onDestroy() {
-        super.onDestroy();
-        show_Toast("Destroy now");
-        Log.d("test1","Destroy now");
-    }
-    public void onSaveInstanceState(Bundle savedInstanceState) {
-        super.onSaveInstanceState(savedInstanceState);
-        show_Toast("onSaveInstance");
-        Log.d("test1","OnSaveInstance");
-    }
-
 
     }
 
