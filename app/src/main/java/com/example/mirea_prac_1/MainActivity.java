468ead3c3aad3aae07f59b31838a891375be22c4
Semyon <semyonkarabanov@gmail.com>
2024-03-11 10:52:32 +0300

    init

--- app/src/main/java/com/example/mirea_prac_1/MainActivity.java
+++ app/src/main/java/com/example/mirea_prac_1/MainActivity.java
@@ -2,57 +2,40 @@ package com.example.mirea_prac_1;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.constraintlayout.widget.ConstraintLayout;
-import androidx.constraintlayout.widget.ConstraintSet;
 
-import android.app.ActionBar;
-import android.content.Intent;
-import android.os.Build;
 import android.os.Bundle;
-import android.util.Log;
-import android.util.TypedValue;
+import android.view.Gravity;
 import android.view.View;
-import android.view.ViewGroup;
 import android.widget.Button;
-import android.widget.EditText;
 import android.widget.TextView;
-import android.widget.Toast;
-
-
 public class MainActivity extends AppCompatActivity {
-
+    int clicks = 0;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.second_layout);
-
-
         ConstraintLayout constraintLayout = new ConstraintLayout(this);
-        EditText editText = new EditText(this);
-        editText.setHint("Введите Email");
-        editText.setId(View.generateViewId());
+        TextView textView = new TextView(this);
+        textView.setText("Hello Android!");
+        textView.setTextSize(30);
+        textView.setBackgroundColor(0xffe8eaf6);
 
-        Button button = new Button(this);
-        button.setText("Отправить");
-        button.setId(View.generateViewId());
+        // установка gravity
+        textView.setGravity(Gravity.BOTTOM | Gravity.CENTER);
+        // установка высоты и ширины
+        ConstraintLayout.LayoutParams layoutParams = new ConstraintLayout.LayoutParams
+                (ConstraintLayout.LayoutParams.MATCH_CONSTRAINT, 200);
 
-        ConstraintLayout.LayoutParams editTextLayout = new ConstraintLayout.LayoutParams
-                (ConstraintLayout.LayoutParams.WRAP_CONTENT , ConstraintLayout.LayoutParams.WRAP_CONTENT);
-        editTextLayout.leftToLeft = ConstraintLayout.LayoutParams.PARENT_ID;
-        editTextLayout.topToTop = ConstraintLayout.LayoutParams.PARENT_ID;
-        editTextLayout.rightToLeft = button.getId();
-        editText.setLayoutParams(editTextLayout);
-        constraintLayout.addView(editText);
+        layoutParams.leftToLeft = ConstraintLayout.LayoutParams.PARENT_ID;
+        layoutParams.rightToRight = ConstraintLayout.LayoutParams.PARENT_ID;
+        layoutParams.topToTop = ConstraintLayout.LayoutParams.PARENT_ID;
+        layoutParams.bottomToBottom = ConstraintLayout.LayoutParams.PARENT_ID;
 
-        ConstraintLayout.LayoutParams buttonLayout = new ConstraintLayout.LayoutParams
-                (ConstraintLayout.LayoutParams.WRAP_CONTENT , ConstraintLayout.LayoutParams.WRAP_CONTENT);
-        buttonLayout.leftToRight = editText.getId();
-        buttonLayout.topToTop = ConstraintLayout.LayoutParams.PARENT_ID;
-        button.setLayoutParams(buttonLayout);
-        constraintLayout.addView(button);
+        textView.setLayoutParams(layoutParams);
+        constraintLayout.addView(textView);
         setContentView(constraintLayout);
 
     }
+}
 
-    }
 
 
